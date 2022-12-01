6ce2064c6677c9b662e2063369e1c818767a5d37
Kyle Kochanek <the_designer@kylekochanek.com>
2022-12-01 14:09:54 -0500

    finished styled components

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -3,10 +3,10 @@ import Link from 'next/link';
 export default function Nav() {
   return (
     <nav>
-      <Link href="./products">Products</Link>
-      <Link href="./sell">Pell</Link>
-      <Link href="./orders">Orders</Link>
-      <Link href="./account">Account</Link>
+      <Link href="/products">Products</Link>
+      <Link href="/sell">Pell</Link>
+      <Link href="/orders">Orders</Link>
+      <Link href="/account">Account</Link>
     </nav>
   );
 }
